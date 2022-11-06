e0f21b379d2110c694d7816e367197d0ec329ed0
tom <ton.ix.tom@gmail.com>
2022-11-06 08:52:44 +0800

    # test add

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,4 +2,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("tom");
     }
+
+    public void test(){
+        System.out.println("tom");
+    }
 }
